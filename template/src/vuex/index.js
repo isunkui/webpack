accd418e4fadf508a5b2eeec39c7de9235c80750
isunkui <fxkui.2011@gmail.com>
2017-11-30 10:51:02 +0800

    - Fix: build error bugs.

--- template/src/vuex/index.js
+++ template/src/vuex/index.js
@@ -7,14 +7,14 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Vuex from 'vuex'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import modules from './module'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import createLogger from 'vuex/logger'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+// import createLogger from 'vuex/logger'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default new Vuex.Store({
   // state: {},
   // can not judge run time environment
-  plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
+  // plugins: process.env.NODE_ENV !== 'production' ? [createLogger()] : [],
   modules,
   strict: false{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
--- template/src/vuex/module/common/index.js
+++ template/src/vuex/module/common/index.js
@@ -6,10 +6,10 @@ const state = {
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 const mutations = {
-  [types.LOADING_START](state) {
+  [types.LOADING_START]{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(state) {
     state.loading = true{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   },
-  [types.LOADING_DONE](state) {
+  [types.LOADING_DONE]{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}(state) {
     state.loading = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
